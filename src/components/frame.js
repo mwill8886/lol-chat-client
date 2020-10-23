434bea0f92139e22031b3f5b694e20e0b4bea1d6
Michael H Will <mike.will@Eclypses.com>
2020-10-22 23:59:23 -0600

    feat: extracted status selector. use local state for status.

--- src/components/frame.js
+++ src/components/frame.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Component Imports
+import StatusSelector from './status-selector';
+
 // Images
 import send from '../images/send.png';
 import sending from '../images/sending.png';
@@ -10,20 +13,7 @@ import sending from '../images/sending.png';
 const Frame = () => {
     return(
         <ComponentWrapper className="bevel">
-            <Select className="bevel">
-                <Selection>
-                    <Indicator color="#40C28D"/>
-                    <span>Im eating pizza</span>
-                </Selection>
-                <DropdownButton className="bevel-reverse">
-                    <Arrow/>
-                </DropdownButton>
-                {/* <DropdownSelection className="bevel">
-                    <div><Indicator color="#DF251D"/>Offline</div>
-                    <div><Indicator color="#F8A811"/>Busy</div>
-                    <div><Indicator color="#40C28D"/>Eating Pizza</div>
-                </DropdownSelection> */}
-            </Select>
+            <StatusSelector />
             <ChatWindow className="bevel">
                 <Messege>
                     <User>SomebodyElse</User>
@@ -64,73 +54,6 @@ const ComponentWrapper = styled.div`
     background-color: #c3c3c3;
 `;
 
-const Select = styled.div`
-    min-height: 38px;
-    margin-bottom: 11px;
-    background-color: #c3c3c3;
-    display: flex;
-    position: relative;
-`;
-
-const Selection = styled.div`
-    flex: 1;
-    display: flex;
-    padding: 8px;
-    align-items: center;
-    line-height: 0px;
-`;
-
-const Indicator = styled.div`
-    display: inline-block;
-    width: 8px;
-    height: 8px;
-    margin-right: 8px;
-    border-radius: 100%;
-    background-color: ${props => props.color};
-`;
-
-const DropdownButton = styled.div`
-    cursor: pointer;
-    width: 36px;
-    height: 36px;
-    margin-right: 2px;
-    margin-bottom: 2px;
-    background-color: #E5E5E5;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    &:hover {
-        background-color: #cccccc;
-    }
-`;
-
-const DropdownSelection = styled.div`
-    position: absolute;
-    padding: 8px;
-    top: 45px;
-    left: -4px;
-    right: -4px;
-    min-height: 100px;
-    background-color: #C3C3C3;
-
-    & > div {
-        padding-top: 5px;
-        padding-bottom: 5px;
-
-        &:hover {
-            background-color: rgba(0,0,0,0.1);
-        }
-    }
-`;
-
-const Arrow = styled.div`
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 8px 7.5px 0 7.5px;
-    border-color: #000000 transparent transparent transparent;
-`;
 
 
 const Messege = styled.div`
---
+++ src/components/status-selector.js
@@ -0,0 +1,112 @@
+// Dependencies
+import React, { useEffect, useState} from 'react';
+import styled from 'styled-components';
+
+const StatusArr = [
+    { state: 'Im offline', color: '#DF251D'}, 
+    {state: 'Im busy', color: '#F8A811'},
+    {state: 'Im eating pizza', color: '#40C28D'}
+];
+
+
+// Component
+const StatusSelector = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [status, setStatus] = useState(StatusArr[2]);
+
+    const handleSelect = (input) => {
+        const newStatus = StatusArr[`${input}`]
+        setStatus(newStatus);
+        // update state with server
+    }
+
+    return(
+        <Select className="bevel" onClick={() => setIsOpen(!isOpen)}>
+            <Selection>
+                <Indicator color={status.color}/>
+                <span>{status.state}</span>
+            </Selection>
+            <DropdownButton className="bevel-reverse" >
+                <Arrow/>
+            </DropdownButton>
+            {
+                isOpen &&
+                <DropdownSelection className="bevel">
+                    <div onClick={() => handleSelect(0)}><Indicator color={StatusArr[0].color}/>Offline</div>
+                    <div onClick={() => handleSelect(1)}><Indicator color={StatusArr[1].color}/>Busy</div>
+                    <div onClick={() => handleSelect(2)}><Indicator color={StatusArr[2].color}/>Eating Pizza</div>
+                </DropdownSelection>
+            }
+        </Select>
+    );
+}
+
+export default StatusSelector;
+
+
+// Styles
+
+const Select = styled.div`
+    cursor:pointer;
+    min-height: 38px;
+    margin-bottom: 11px;
+    background-color: #c3c3c3;
+    display: flex;
+    position: relative;
+`;
+
+const Selection = styled.div`
+    flex: 1;
+    display: flex;
+    padding: 8px;
+    align-items: center;
+    line-height: 0px;
+`;
+
+const Indicator = styled.div`
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    margin-right: 8px;
+    border-radius: 100%;
+    background-color: ${props => props.color};
+`;
+
+const DropdownButton = styled.div`
+    width: 36px;
+    height: 36px;
+    margin-right: 2px;
+    margin-bottom: 2px;
+    background-color: #E5E5E5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        background-color: #cccccc;
+    }
+`;
+
+const DropdownSelection = styled.div`
+    position: absolute;
+    top: 45px;
+    left: -4px;
+    right: -4px;
+    min-height: 100px;
+    background-color: #C3C3C3;
+
+    & > div {
+        padding: 5px 8px;
+        &:hover {
+            background-color: rgba(0,0,0,0.1);
+        }
+    }
+`;
+
+const Arrow = styled.div`
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 8px 7.5px 0 7.5px;
+    border-color: #000000 transparent transparent transparent;
+`;
